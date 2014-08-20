370613c6491eaea7035ffa022420de3787cf94bf
Reggie Jones <reggiejones7@hotmail.com>
2014-08-20 09:34:13 -0600

    refining the rule to only look for script tags

--- src/main/java/com/cflint/plugins/core/ScriptTagChecker.java
+++ src/main/java/com/cflint/plugins/core/ScriptTagChecker.java
@@ -22,32 +22,19 @@ public class ScriptTagChecker implements CFLintScanner {
 	public void expression(final CFScriptStatement expression, final Context context, final BugList bugs) {
 	
 	}
-
+	
+	//rule: don't use inline javascript in cfm and cfc files
 	public void element(final Element element, final Context context, final BugList bugs) {
-		//changing this for <script> and <style> tag, change the file name accordingly if we keep it
 		if (element.getName().equals("script")) {  
 			int endLine = element.getSource().getRow(element.getEnd()); 
 			int begLine = element.getSource().getRow(element.getBegin());
 			int total = endLine - begLine;
 			if (total > SCRIPT_LENGTH_THRESHOLD) {
-				bugs.add(new BugInfo.BugInfoBuilder().setLine(begLine).setMessageCode("QUERYPARAM_REQ")
-						.setSeverity(CFSeverity.INFO.getValue()).setFilename(context.getFilename())
+				bugs.add(new BugInfo.BugInfoBuilder().setLine(begLine).setMessageCode("AVOID_USING_INLINE_JS")
+						.setSeverity(CFSeverity.WARNING.getValue()).setFilename(context.getFilename())
 						.setMessage(element.getName() + " is too long, consider making it a <cfmcjavascript> tag")
 						.build());
 			}
 		}
-
-		if (element.getName().equals("style")) {  
-			int endLine = element.getSource().getRow(element.getEnd()); 
-			int begLine = element.getSource().getRow(element.getBegin());
-			int total = endLine - begLine;
-			if (total > SCRIPT_LENGTH_THRESHOLD) {
-				bugs.add(new BugInfo.BugInfoBuilder().setLine(begLine).setMessageCode("QUERYPARAM_REQ")
-						.setSeverity(CFSeverity.INFO.getValue()).setFilename(context.getFilename())
-						.setMessage(element.getName() + " is too long, consider making it a <cfmccss> tag")
-						.build());
-			}
-		}
 	}
-
 }
